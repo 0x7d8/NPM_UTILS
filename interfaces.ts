aa89288c5356198bbb8389ed24bb0a08e14dea9f
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2023-01-15 14:56:23 +0100

    [1.1.1] #0 - EXPORT INTERFACES

---
+++ interfaces.ts
@@ -0,0 +1,4 @@
+export { default as decryptStrOptions } from "./src/interfaces/decryptStrOptions"
+export { default as encryptStrOptions } from "./src/interfaces/encryptStrOptions"
+export { default as hashStrOptions } from "./src/interfaces/hashStrOptions"
+export { default as randomStrOptions } from "./src/interfaces/randomStrOptions"
